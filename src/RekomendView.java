29f476ec656130b8ee9369e2ac26e581235de879
tomyc <tomasz@cieplak.net>
2016-07-06 11:51:15 +0200

    Nowa wersja

--- src/RekomendView.java
+++ src/RekomendView.java
@@ -1,11 +1,39 @@
 import javax.swing.*;
+import java.awt.event.ActionListener;
 
 /**
  * Created by Tomasz on 06.07.2016.
  */
-public class RekomendView {
-    private JTextField textField1;
-    private JTextField textField2;
-    private JButton znajdźButton;
-    private JTextPane wynikiWyszukiwaniaTextPane;
+public class RekomendView extends JFrame {
+    private JTextField regionTF;
+    private JTextField wartoscTF;
+    private JButton znajdzButton;
+    private JTextPane wynikiWyszukiwaniaTP;
+    private JPanel rekomendPanel;
+
+    RekomendView(){
+        this.add(rekomendPanel);
+        this.setSize(600,400);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public String getRegion(){
+        return regionTF.getText();
+    }
+
+    public void setWynikiWyszukiwaniaTP(String miejsce){
+        wynikiWyszukiwaniaTP.setText(miejsce);
+    }
+
+    void addZnajdzListener(ActionListener listenerForZnajdzBtn){
+        znajdzButton.addActionListener(listenerForZnajdzBtn);
+    }
+
+    void displayErrorMessage(String errorMessage){
+        JOptionPane.showMessageDialog(this,errorMessage);
+    }
+
+
+
+
 }
